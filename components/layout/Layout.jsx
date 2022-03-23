f0b62df88860482b5cc8b39fa693cc47d9ea0a12
Cody <relationalprogrammer@gmail.com>
2022-03-23 10:19:46 -0500

    added Layout component for header & nav

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MainHeader from "./MainHeader";
+
+
+export default function Layout(props) {
+  return (
+    <Fragment>
+        <MainHeader />
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
+  )
+}
