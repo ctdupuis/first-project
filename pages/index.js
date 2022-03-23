779f0137387eb3783a579183c5976508761483fb
Cody <relationalprogrammer@gmail.com>
2022-03-23 11:21:32 -0500

    added a title to the app

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../dummy_data';
 
@@ -5,8 +7,13 @@ export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
-    <div>
-      <EventList events={featuredEvents} />
-    </div>
+    <Fragment>
+      <Head>
+        <title>NextEvents</title>
+      </Head>
+      <div>
+        <EventList events={featuredEvents} />
+      </div>
+    </Fragment>
   )
 }
