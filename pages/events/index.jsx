af7cca2527788a3d6d6bec47406877e04e3125f3
Cody <relationalprogrammer@gmail.com>
2022-03-23 10:36:43 -0500

    code and styling for EventsSearch component

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,15 @@
 import { getAllEvents } from "../../dummy_data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList events={events} />
-    </div>
+    </Fragment>
   )
 }
